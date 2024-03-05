976b612b5382696da779b693f1b301cec7526fe5
Ariel del Rio <arieldelrioviamonte@gmail.com>
2024-03-05 15:55:49 -0500

    Add team collection, add MarkdownWrapper layout, add blog index page, add PostCard component, add blog dynamic route page

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,6 +1,8 @@
 import posts from "./collections/posts.ts";
+import team from "./collections/author.ts";
 
 // 3. Export a single `collections` object to register your collection(s)
 export const collections = {
   posts,
+  team,
 };
