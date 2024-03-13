b623859c1a08d8f4c869d6834f10b2f7bcc22c00
Ariel del Rio <arieldelrioviamonte@gmail.com>
2024-03-12 23:08:31 -0400

    WIP Grid to list layout

---
+++ src/components/index.ts
@@ -0,0 +1,2 @@
+export { default as Tag } from "./Tag/Tag.astro";
+export { default as Card } from "./Card/Card.astro";
---
+++ src/icons/index.ts
@@ -0,0 +1,2 @@
+export { default as Square } from "./Square.astro";
+export { default as QueueList } from "./QueueList.astro";
